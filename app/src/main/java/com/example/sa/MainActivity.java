6ef82b31f5af4dfe4873d9d1428b7fcc731cdbc6
goncalobraga27 <a97541@alunos.uminho.pt>
2024-04-15 17:59:07 +0100

    Services Location

--- app/src/main/java/com/example/sa/MainActivity.java
+++ app/src/main/java/com/example/sa/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.sa;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.location.Geocoder;
 import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
 import android.os.ResultReceiver;
 import android.util.Log;
 import android.widget.Toast;
@@ -45,6 +48,7 @@ import java.net.Inet4Address;
 public class MainActivity extends AppCompatActivity {
     FusedLocationProviderClient client;
     AddressResultReceiver resultReceiver;
+    private static final int REQUEST_LOCATION_PERMISSION = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         client = LocationServices.getFusedLocationProviderClient(this);
-        resultReceiver= new AddressResultReceiver(null);
+        //resultReceiver= new AddressResultReceiver(null);
 
 
     }
@@ -75,15 +79,8 @@ public class MainActivity extends AppCompatActivity {
                 break;
 
         }
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION);
         }
         client.getLastLocation()
                 .addOnSuccessListener(new OnSuccessListener<Location>() {
@@ -103,7 +100,7 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
         LocationRequest locationRequest;
-        locationRequest = new LocationRequest();
+        locationRequest = LocationRequest.create();
         locationRequest.setInterval(15*1000);
         locationRequest.setFastestInterval(5*1000);
         locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
@@ -154,7 +151,7 @@ public class MainActivity extends AppCompatActivity {
                 super.onLocationAvailability(locationAvailability);
             }
         };
-        client.requestLocationUpdates(locationRequest,locationCallback,null);
+        client.requestLocationUpdates(locationRequest,locationCallback, Looper.getMainLooper());
     }
 
     private class AddressResultReceiver extends ResultReceiver{
